f645e296ad5070d9d137f794309e8dfb6d6509d4
Dedeyd70 <dorothyokoto@gmail.com>
2024-01-24 08:38:10 +0000

    This commit includes updates to various files in the Inventory_Xpress

--- server.js
+++ server.js
@@ -185,14 +185,12 @@ app.post('/login-user', async (req, res) => {
 
 // API endpoint to get products
 app.get('/api/products', (req, res) => {
-  const userId = req.session.userId;
-
- db('products').select('*').where('user_id', userId)
-   .then((results) => res.json(results))
-   .catch((err) => {
-     console.error('Error getting products:', err);
-     res.status(500).send('Internal Server Error');
-   });
+  db('products').select('*')
+    .then((results) => res.json(results))
+    .catch((err) => {
+      console.error('Error getting products:', err);
+      res.status(500).send('Internal Server Error');
+    });
 });
 
 
